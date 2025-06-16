5104f50f3cc2a4b19188b3d02ddb6b00b3a1c5f9
preksha3005 <preksha.g504@gmail.com>
2025-06-16 19:04:04 +0530

    Update backend

--- server/index.js
+++ server/index.js
@@ -265,12 +265,12 @@ app.delete("/del/:id", verifyuser, async (req, res) => {
     .catch((err) => console.log(err));
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"))
-  );
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../client/build")));
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "../client/build", "index.html"))
+//   );
+// }
 
 app.listen(process.env.PORT, () => {
   console.log("Server is running");
