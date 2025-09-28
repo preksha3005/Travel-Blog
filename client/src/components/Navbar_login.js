48d0c654b04aa62b8b290985c9c1e290a5f60478
preksha3005 <preksha.g504@gmail.com>
2025-09-28 19:09:50 +0530

    modified navbar

--- client/src/components/Navbar_login.js
+++ client/src/components/Navbar_login.js
@@ -30,7 +30,9 @@ const Navbar_login = () => {
   return (
     <nav className="w-full h-14 flex justify-between text-lg font-bold bg-blue-400/30 backdrop-blur-sm p-2 xs:text-base xs:h-10 xs:w-full md:text-2xl lg:text-2xl">
       <ul className="navbar">
-        <li>Travel Blog</li>
+        <li><a className="hover:text-blue-800" href="/create">
+           Travel Blog
+          </a></li>
       </ul>
 
       <ul className="flex relative -left-5 gap-20 text-xl">
